4b496eee2c74683e0267e0b6eee68c57991c23c1
Fernando Ventura <ferventurart@gmail.com>
2020-05-13 09:56:41 -0600

    Create Web Advert Api

---
+++ AdvertApi/Controllers/AdvertController.cs
@@ -0,0 +1,65 @@
+﻿using AdvertApi.Models;
+using AdvertApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AdvertApi.Controllers
+{
+    [Route("adverts/v1")]
+    [ApiController]
+    public class AdvertController : ControllerBase
+    {
+        private readonly IAdvertStorageService _advertStorageService;
+
+        public AdvertController(IAdvertStorageService advertStorageService)
+        {
+            _advertStorageService = advertStorageService;
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(200, Type = typeof(CreateAdvertResponse))]
+        public async Task<IActionResult> Create(AdvertModel model)
+        {
+            string recordId;
+            try
+            {
+                recordId = await _advertStorageService.Add(model);
+            }
+            catch (KeyNotFoundException)
+            {
+                return new NotFoundResult();
+            }
+            catch (Exception exception)
+            {
+                return StatusCode(500, exception.Message);
+            }
+
+            return StatusCode(201, new CreateAdvertResponse { Id = recordId });
+        }
+
+        [HttpPut]
+        [Route("Confirm")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200)]
+        public async Task<IActionResult> Confirm(ConfirmAdvertModel model)
+        {
+            try
+            {
+                await _advertStorageService.Confirm(model);
+            }
+            catch (KeyNotFoundException)
+            {
+                return new NotFoundResult();
+            }
+            catch (Exception exception)
+            {
+                return StatusCode(500, exception.Message);
+            }
+            return new OkResult();
+        }
+    }
+}
---
+++ AdvertApi/Services/IAdvertStorageService.cs
@@ -0,0 +1,12 @@
+﻿using AdvertApi.Models;
+using System.Threading.Tasks;
+
+namespace AdvertApi.Services
+{
+    public interface IAdvertStorageService
+    {
+        Task<string> Add(AdvertModel model);
+        Task Confirm(ConfirmAdvertModel model);
+        Task<bool> CheckHealthAsync();
+    }
+}
